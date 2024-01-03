9c7348867e07f78f9ef0fdcd00eec18086252e4c
Vincent Song <dela.vincent.15@gmail.com>
2024-01-03 14:52:50 -0500

    Add socials

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,54 +1,45 @@
-import { Typography, Button, Input } from "@material-tailwind/react";
+import { Typography, Button } from "@material-tailwind/react";
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons CSS
 
-const LINKS = ["About Us", "Careers", "Press", "Blog", "Pricing"];
-const SUB_LINKS = ["Claim", "Privacy", "Terms"];
+const LINKS = [
+  { name: "Instagram", icon: "bi-instagram" },
+  { name: "LinkedIn", icon: "bi-linkedin" },
+  { name: "YouTube", icon: "bi-youtube" },
+  { name: "Discord", icon: "bi-discord" },
+];
 const CURRENT_YEAR = new Date().getFullYear();
 
 export function Footer() {
   return (
-    <footer className="mt-10 px-8 pt-20">
+    <footer>
       <div className="container mx-auto">
-        <div className="flex flex-wrap items-end justify-center gap-8 md:justify-between">
-          <div className="text-center md:text-left">
-            <Typography
-              as="a"
-              href="https://www.material-tailwind.com"
-              target="_blank"
-              variant="h4"
-              className="mb-6"
-            >
-              Material Tailwind
-            </Typography>
-            <ul className="flex flex-wrap items-center justify-center md:justify-start">
-              {LINKS.map((link, idx) => (
-                <li key={link}>
-                  <Typography
-                    as="a"
-                    href="#"
-                    className={`py-1 font-medium !text-gray-700 transition-colors hover:!text-gray-900 ${
-                      idx === 0 ? "pr-3" : "px-3"
-                    }`}
-                  >
-                    {link}
-                  </Typography>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="w-full sm:w-[24rem] sm:min-w-[24rem]">
-            
-          </div>
-        </div>
-        <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 gap-x-8 border-t border-blue-gray-50 py-6 md:justify-between">
+        <div className="flex flex-wrap items-center justify-center gap-y-4 gap-x-8 border-t border-blue-gray-50 py-6 md:justify-between">
           <Typography className="text-center font-normal !text-gray-700">
             &copy; {CURRENT_YEAR} UVA Google Developer Student Club
           </Typography>
-
+          <ul className="flex flex-wrap items-center justify-center">
+            {LINKS.map((link) => (
+              <li key={link.name}>
+                <a
+                  href="#"
+                  className="flex items-center py-1 font-medium !text-gray-700 transition-colors hover:!text-gray-900 px-6"
+                >
+                  <i className={link.icon}></i>
+                  <Typography as="span" className="pl-2 hidden">
+                    {link.name}
+                  </Typography>
+                </a>
+              </li>
+            ))}
+          </ul>
           <ul className="flex items-center">
-            <div className="flex flex-col gap-3 sm:flex-row">
-              {/* @ts-ignore */}
-              <a href="https://lists.virginia.edu/sympa/subscribe/google_dsc?previous_action=info" target="_blank" rel="noopener noreferrer">
-                <Button color="gray" className="flex-shrink-0">
+            <div className="flex gap-3 flex-col">
+              <a
+                href="https://lists.virginia.edu/sympa/subscribe/google_dsc?previous_action=info"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button color="gray">
                   Join Our Mailing List
                 </Button>
               </a>
