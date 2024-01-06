f0e9db099c8b5f976e644023d5b917a13b7e5028
Vincent Song <dela.vincent.15@gmail.com>
2024-01-06 15:02:03 -0500

    Fix hero footer

--- src/components/home_footer.tsx
+++ src/components/home_footer.tsx
@@ -28,34 +28,33 @@ const CURRENT_YEAR = new Date().getFullYear();
 
 export function HomeFooter() {
   return (
-    <footer className="absolute bottom-0 w-full">
+    <footer className="absolute bottom-0 z-10 w-full">
       <div className="container mx-auto">
         <div className="grid items-center justify-center gap-y-4 gap-x-8 border-blue-gray-50 py-6 xl:justify-between grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
-          <div className="col-span-1">
-            <Typography className="text-center font-normal !text-white/40 whitespace-nowrap">
-              &copy; {CURRENT_YEAR} UVA Google Developer Student Club
-            </Typography>
-          </div>
-          <div className="col-span-1">
-            <ul className="flex items-center justify-center ">
-              {LINKS.map((link) => (
-                <li key={link.name}>
-                  <a
-                    href={link.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center py-1 font-medium !text-white/40 transition-colors hover:!text-gray-900 px-6"
-                  >
-                    <i className={link.icon}></i>
-                    <Typography as="span" className="pl-2 hidden">
-                      {link.name}
-                    </Typography>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="col-span-1 bg-blue-300" />
+        <Typography className="text-center font-normal !text-gray-700">
+            &copy; {CURRENT_YEAR} UVA Google Developer Student Club
+          </Typography>
+          <ul className="flex flex-wrap items-center justify-center">
+          {LINKS.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center py-1 font-medium !text-gray-700 transition-colors hover:!text-gray-900 px-6"
+                >
+                  <i className={link.icon}></i>
+                  <Typography as="span" className="pl-2 hidden">
+                    {link.name}
+                  </Typography>
+                </a>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex items-center">
+            <div className="flex gap-3 flex-col">
+            </div>
+          </ul>
         </div>
       </div>
     </footer>
